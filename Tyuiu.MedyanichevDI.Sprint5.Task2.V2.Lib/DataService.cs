bc5648110f3359515c67315d00b5d92391bbbc09
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-11-11 22:28:47 +0500

    Task1

---
+++ Tyuiu.MedyanichevDI.Sprint5.Task2.V2.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.MedyanichevDI.Sprint5.Task2.V2.Lib
+{
+    public class DataService : ISprint5Task1V2
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists) { File.Delete(path); }
+
+            double res;
+            string strRes;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                res = (2 * x - 3) / Math.Cos(x) - 2 * x;
+                strRes = Convert.ToString(res);
+
+                if (Math.Cos(x) - 2 * x == 0)
+                {
+                    File.AppendAllText(path, "0");
+                }
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strRes + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strRes);
+                }
+            }
+            return path;
+        }
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint5.Task2.V2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Tyuiu.MedyanichevDI.Sprint5.Task2.V2.Lib;
+
+namespace Tyuiu.MedyanichevDI.Sprint5.Task2.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Медяничев Д.И. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #2                                                             *");
+            Console.WriteLine("* Выполнил: Медяничев Денис Игоревич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3,                        *");
+            Console.WriteLine("* результат сохранить в текстовый файл OutPutFileTask0.txt                *");
+            Console.WriteLine("* и вывести на консоль. Округлить до трёх знаков после запятой.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int a = -5;
+            int b = 5;
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(a,b);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
