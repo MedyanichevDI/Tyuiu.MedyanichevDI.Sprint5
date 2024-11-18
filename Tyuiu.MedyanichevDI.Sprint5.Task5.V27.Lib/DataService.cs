06b00e80d04241ca1e8da478f8568dda6427474b
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-11-18 11:53:10 +0500

    Task5

---
+++ Tyuiu.MedyanichevDI.Sprint5.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MedyanichevDI.Sprint5.Task5.V27.Lib
+{
+    public class DataService : ISprint5Task5V27
+    {
+        public double LoadFromDataFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                List<double> positiveNumbers = new List<double>();
+
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    var values = line.Split(new[] { ' ', ',', ';', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                    foreach (var value in values)
+                    {
+                        if (double.TryParse(value, out double number) && number / 5 == 0)
+                        {
+                            positiveNumbers.Add(number);
+                        }
+                    }
+                }
+                return Math.Round(positiveNumbers.Average(), 3);
+            }
+        }
+    }
+}
+
---
+++ Tyuiu.MedyanichevDI.Sprint5.Task5.V27/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.MedyanichevDI.Sprint5.Task5.V27.Lib;
+
+namespace Tyuiu.MedyanichevDI.Sprint5.Task5.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path;
+            Console.Title = "Спринт #5 | Выполнил: Медяничев Денис Игоревич | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #27                                                              *");
+            Console.WriteLine("* Выполнил: Медяничев Денис Игоревич| АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите путь к файлу:");
+
+            path = Console.ReadLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.LoadFromDataFile(path));
+        }
+    }
+}
